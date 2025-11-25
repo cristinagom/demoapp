e0717c55761e59ec3a2c45235377efec0d85c441
cristinag <gomezalonso.cristina@gmail.com>
2025-11-25 14:40:24 +0100

    feat: add saludoform

--- src/main/java/demoapp/controller/SaludoController.java
+++ src/main/java/demoapp/controller/SaludoController.java
@@ -20,6 +20,7 @@ public class SaludoController {
 
     @RequestMapping("/saludo/{nombre}")
     public @ResponseBody String saludo(@PathVariable(value="nombre") String nombre) {
+
         return service.saluda(nombre);
     }
 
---
+++ src/main/java/demoapp/controller/SaludoControllerForm.java
@@ -0,0 +1,35 @@
+package demoapp.controller;
+
+import demoapp.dto.UserData;
+import demoapp.service.SaludoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class SaludoControllerForm {
+
+    @Autowired
+    private SaludoService service;
+
+    @GetMapping("/saludoform")
+    // Hay que declarar un parámetro con el tipo usado en el modelo del formulario (UserData)
+    public String saludoForm(UserData userData) {
+        return "formRegistro";
+    }
+
+    @PostMapping("/saludoform")
+    public String checkPersonInfo(@ModelAttribute @Valid UserData userData, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "formRegistro";
+        }
+        model.addAttribute("mensaje", service.saluda(userData.getNombre()));
+        return "saludo";
+    }
+}
+
